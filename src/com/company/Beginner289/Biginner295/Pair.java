f03237131a6b19ee2b66357359148c265412daac
Vaibhav Gawad <58821506+vaibhav31s@users.noreply.github.com>
2023-03-25 23:30:57 +0530

    biginner 289 turned into orange finally ::|)

---
+++ src/com/company/Beginner289/Biginner295/Pair.java
@@ -0,0 +1,10 @@
+package com.company.Beginner289.Biginner295;
+
+public class Pair<T, T1> {
+    int a = 0;
+    int b = 0;
+    Pair(Integer t, Integer rr){
+        a = t;
+        b = rr;
+    }
+}
